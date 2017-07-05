94eadb1707158988a5eaa516d08ba504407d5a67
Patrick Robinson <patrick.robinson@envato.com>
2017-07-05 23:14:08 +1000

    Recieves inputs from Kinesis

---
+++ input/kinesis.go
@@ -0,0 +1,143 @@
+package input
+
+import (
+	"errors"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/aws/aws-sdk-go/service/kinesis/kinesisiface"
+)
+
+// KinesisInput implements the Input interface
+type KinesisInput struct {
+	outputChan *chan []byte
+	shardIds   map[string]shardStatus
+	shardMgmt  *chan shardChange
+	streamName string
+	kinesisSvc kinesisiface.KinesisAPI
+}
+
+type shardStatus struct {
+	shardID    string
+	checkpoint string
+}
+
+type shardChange struct {
+	shardID string
+	action  string
+}
+
+// Init implements intialises the Input mechanism
+func (ki *KinesisInput) Init(streamName string) error {
+	ki.streamName = streamName
+	session, err := session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable})
+	if err != nil {
+		return err
+	}
+	kinesisSvc := kinesis.New(session)
+	shardIds, err := getShardIds(kinesisSvc, streamName, "")
+	ki.shardIds = shardIds
+
+	shardChan := make(chan shardChange)
+	ki.shardMgmt = &shardChan
+	go ki.shardIDManager()
+	return err
+}
+
+func getShardIds(svc kinesisiface.KinesisAPI, streamName string, startShardID string) (map[string]shardStatus, error) {
+	shards := map[string]shardStatus{}
+	args := &kinesis.DescribeStreamInput{
+		StreamName: aws.String(streamName),
+	}
+	streamDesc, err := svc.DescribeStream(args)
+	if err != nil {
+		return shards, err
+	}
+
+	if *streamDesc.StreamDescription.StreamStatus != "ACTIVE" {
+		return shards, errors.New("Stream not active")
+	}
+
+	var lastShardID string
+	for _, s := range streamDesc.StreamDescription.Shards {
+		shards[*s.ShardId] = shardStatus{
+			shardID: *s.ShardId,
+		}
+		lastShardID = *s.ShardId
+	}
+
+	if *streamDesc.StreamDescription.HasMoreShards {
+		moreShards, err := getShardIds(svc, streamName, lastShardID)
+		if err != nil {
+			return shards, err
+		}
+		for k, v := range moreShards {
+			shards[k] = v
+		}
+	}
+
+	return shards, nil
+}
+
+func (ki *KinesisInput) shardIDManager() {
+	for {
+		select {
+		case shardChange := <-*ki.shardMgmt:
+			if shardChange.action != "DELETE" {
+				log.Errorf("Invalid action received to shardID Manager: %v", shardChange.action)
+			}
+			delete(ki.shardIds, shardChange.shardID)
+		default:
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}
+}
+
+func (ki *KinesisInput) getRecords(shardID string, output *chan []byte) {
+	svc := ki.kinesisSvc
+	shardIterArgs := &kinesis.GetShardIteratorInput{
+		ShardId:           aws.String(shardID),
+		ShardIteratorType: aws.String("TRIM_HORIZON"),
+		StreamName:        aws.String(ki.streamName),
+	}
+	iterResp, err := svc.GetShardIterator(shardIterArgs)
+	if err != nil {
+		log.Fatalf("Unable to retrieve records: %v", err)
+	}
+	shard := ki.shardIds[shardID]
+	shard.checkpoint = *iterResp.ShardIterator
+
+	for {
+		getRecordsArgs := &kinesis.GetRecordsInput{
+			ShardIterator: aws.String(shard.checkpoint),
+		}
+		getResp, err := svc.GetRecords(getRecordsArgs)
+		if err != nil {
+			log.Errorf("Error getting records from shard %v: %v", shardID, err)
+			continue
+		}
+		if getResp.NextShardIterator == nil {
+			*ki.shardMgmt <- shardChange{
+				shardID: shardID,
+				action:  "DELETE",
+			}
+		} else {
+			shard.checkpoint = *getResp.NextShardIterator
+		}
+
+		for i := range getResp.Records {
+			*ki.outputChan <- getResp.Records[i].Data
+		}
+	}
+}
+
+// Retrieve implements the Input interface, it starts the Kinesis Client Library processing
+func (ki *KinesisInput) Retrieve(output *chan []byte) {
+	ki.outputChan = output
+	for _, s := range ki.shardIds {
+		go ki.getRecords(s.shardID, output)
+	}
+}
---
+++ input/kinesis_test.go
@@ -0,0 +1,121 @@
+package input
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/aws/aws-sdk-go/service/kinesis/kinesisiface"
+)
+
+func setupInput(mockClient kinesisiface.KinesisAPI) *KinesisInput {
+	outChan := make(chan []byte)
+	mgmtChan := make(chan shardChange)
+	return &KinesisInput{
+		outputChan: &outChan,
+		shardMgmt:  &mgmtChan,
+		streamName: "testStream",
+		kinesisSvc: mockClient,
+		shardIds: map[string]shardStatus{
+			"00000001": shardStatus{
+				shardID: "00000001",
+			},
+		},
+	}
+}
+
+type mockKClientGet struct {
+	kinesisiface.KinesisAPI
+}
+
+func (k *mockKClientGet) GetShardIterator(args *kinesis.GetShardIteratorInput) (*kinesis.GetShardIteratorOutput, error) {
+	return &kinesis.GetShardIteratorOutput{
+		ShardIterator: aws.String("0123456789ABCDEF"),
+	}, nil
+}
+
+func (k *mockKClientGet) GetRecords(args *kinesis.GetRecordsInput) (*kinesis.GetRecordsOutput, error) {
+	return &kinesis.GetRecordsOutput{
+		MillisBehindLatest: aws.Int64(0),
+		NextShardIterator:  aws.String("ABCD1234"),
+		Records: []*kinesis.Record{
+			&kinesis.Record{
+				Data: []byte("Hello World"),
+			},
+		},
+	}, nil
+}
+func TestSendRecordsToChannel(t *testing.T) {
+	input := setupInput(&mockKClientGet{})
+	inputC := make(chan []byte)
+	go input.Retrieve(&inputC)
+	msg := <-inputC
+	if string(msg) != "Hello World" {
+		t.Errorf("Message received does not match %v", msg)
+	}
+}
+
+type mockKClientClosedShard struct {
+	kinesisiface.KinesisAPI
+}
+
+func (k *mockKClientClosedShard) GetShardIterator(args *kinesis.GetShardIteratorInput) (*kinesis.GetShardIteratorOutput, error) {
+	return &kinesis.GetShardIteratorOutput{
+		ShardIterator: aws.String("0123456789ABCDEF"),
+	}, nil
+}
+
+func (k *mockKClientClosedShard) GetRecords(args *kinesis.GetRecordsInput) (*kinesis.GetRecordsOutput, error) {
+	return &kinesis.GetRecordsOutput{
+		MillisBehindLatest: aws.Int64(0),
+		NextShardIterator:  nil,
+		Records: []*kinesis.Record{
+			&kinesis.Record{
+				Data: []byte("Hello World"),
+			},
+		},
+	}, nil
+}
+
+func TestShardClosed(t *testing.T) {
+	input := setupInput(&mockKClientClosedShard{})
+	inputC := make(chan []byte)
+	go input.shardIDManager()
+	go input.Retrieve(&inputC)
+	m := <-inputC
+	if _, ok := input.shardIds["00000001"]; ok {
+		t.Errorf("Shard not deleted %v", m)
+	}
+}
+
+type mockKClientGetShards struct {
+	kinesisiface.KinesisAPI
+}
+
+func (k *mockKClientGetShards) DescribeStream(args *kinesis.DescribeStreamInput) (*kinesis.DescribeStreamOutput, error) {
+	return &kinesis.DescribeStreamOutput{
+		StreamDescription: &kinesis.StreamDescription{
+			StreamStatus:  aws.String("ACTIVE"),
+			HasMoreShards: aws.Bool(false),
+			Shards: []*kinesis.Shard{
+				&kinesis.Shard{
+					ShardId: aws.String("00000001"),
+				},
+			},
+		},
+	}, nil
+}
+
+func TestGetShardIds(t *testing.T) {
+	client := &mockKClientGetShards{}
+	shards, err := getShardIds(client, "testStream", "")
+	if err != nil {
+		t.Errorf("Error getting Shards: %v", err)
+	}
+	if v, ok := shards["00000001"]; !ok || v.shardID != "00000001" {
+		t.Errorf("Invalid Shard ID returned")
+	}
+	if len(shards) != 1 {
+		t.Errorf("Too many shards")
+	}
+}
