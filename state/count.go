8c4b512be7404c0909c9e7331b2adfc60009de28
Patrick Robinson <patrick.robinson@envato.com>
2017-08-18 21:25:38 +1000

    Add windowing capability

--- state/count.go
+++ state/count.go
@@ -13,6 +13,14 @@ func (c *Counter) Increment() {
 	c.Unlock()
 }
 
+func (c *Counter) Window() int {
+	c.Lock()
+	ret := c.Counter
+	c.Counter = 0
+	c.Unlock()
+	return ret
+}
+
 func NewCounter() *Counter {
 	return &Counter{
 		Counter: 0,
--- state/count_test.go
+++ state/count_test.go
@@ -11,3 +11,16 @@ func TestCounter(t *testing.T) {
 		t.Error("Counter not incremented")
 	}
 }
+
+func TestCounterWindowing(t *testing.T) {
+	counter := NewCounter()
+	(*counter).Increment()
+	(*counter).Increment()
+	(*counter).Increment()
+	if (*counter).Window() != 3 {
+		t.Error("Counter not incremented")
+	}
+	if (*counter).Counter != 0 {
+		t.Error("Expected counter to be zero")
+	}
+}
