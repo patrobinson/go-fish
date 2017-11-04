9bb27e2243fd90aec2e77643194b438d6e325d89
Patrick Robinson <patrick.robinson@envato.com>
2017-11-04 22:46:59 +1100

    Create certstream input for example code

---
+++ input/cert_stream.go
@@ -0,0 +1,32 @@
+package input
+
+import (
+	certstream "github.com/CaliDog/certstream-go"
+	log "github.com/Sirupsen/logrus"
+	"github.com/jmoiron/jsonq"
+)
+
+// CertStreamInput provides a stream of Certificate Transparency Logs https://www.certificate-transparency.org/
+// It's used for example purposes only and should NOT be used in production
+// Since the stream is a websocket slow processing would result in memory pressure
+// Instead the data should be pushed into a queue, such as Kinesis, and GoFish should read from that stream
+type CertStreamInput struct {
+	stream chan jsonq.JsonQuery
+}
+
+func (c *CertStreamInput) Init() error {
+	c.stream = certstream.CertStreamEventStream(false)
+	return nil
+}
+
+func (c *CertStreamInput) Retrieve(output *chan []byte) {
+	defer close(*output)
+	for i := range c.stream {
+		json, err := i.String()
+		if err != nil {
+			log.Errorf("Invalid data from Cert Stream: %s", err)
+			continue
+		}
+		*output <- []byte(json)
+	}
+}
